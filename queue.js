eae5eed52ffb176f194988422723e33184a19d88
Max Meier <max@myownpla.net>
2012-05-24 20:36:11 -0700

    testing update from worqshop

--- queue.js
+++ queue.js
@@ -13,3 +13,11 @@ exports.setInterval = function(interval, callback){
 exports.interval = var interval = function(callback){
 	
 }
+
+exports.loadData = function(data, callback){
+
+}
+
+exports.exportData = function(database, callback){
+
+}
