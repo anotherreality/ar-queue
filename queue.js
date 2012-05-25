22e8436b290a576ae861875712a3622bb7b2f8df
Max Meier <max@myownpla.net>
2012-05-24 20:22:44 -0700

    queue.js added

---
+++ queue.js
@@ -0,0 +1,15 @@
+exports.start = function(callback){
+
+}
+
+exports.stop = function(callback){
+
+}
+
+exports.setInterval = function(interval, callback){
+
+}
+
+exports.interval = var interval = function(callback){
+	
+}
