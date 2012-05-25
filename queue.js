e1681339b2276f125e4d7946073031b376ccfce3
Max Meier <max@myownpla.net>
2012-05-24 21:29:00 -0700

    setup outline for queue tasks

--- queue.js
+++ queue.js
@@ -1,12 +1,27 @@
-var _queue = [];
-var _interval = 60000;
+//
+//  ar-queue.js
+//
+//  Created by Max Meier on 5/24/12.
+//  Copyright (c) 2012 Another Reality. All rights reserved.
+//
 
-exports.start = function(callback){
+var _queue = [];
+var _interval = 60000; //defaults to 1 minute
+
+_run = function(){
 
 }
 
-exports.stop = function(callback){
+_stop = function(){
 
+}
+
+exports.start = function(callback){
+	//begin popping off items and running at set interval
+}
+
+exports.stop = function(callback){
+	
 }
 
 exports.setDispatchInterval = function(interval, callback){
@@ -15,17 +30,18 @@ exports.setDispatchInterval = function(interval, callback){
 }
 
 exports.loadData = function(data, callback){
-
+	
 }
 
 exports.exportData = function(database, callback){
-
+	
 }
 
 exports.addTask = function(callback){
-
+	
+	//return task id or error
 }
 
 exports.removeTask(id, callback(){
-
+	
 }
